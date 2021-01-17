2b9bf6ab33fd30c4738d36d148bd7e6d65bd853a
Yogesh Rathod <yogesh.rathod@benow.in>
2021-01-17 16:33:43 +0530

    Initial App

---
+++ js/main.js
@@ -0,0 +1,272 @@
+$(document).ready(function () {
+  console.log("MAIN JS");
+
+  const youtubeData = {
+    kind: "youtube#searchListResponse",
+    etag: "l1Z8wR19XMILBT-Ju3hlGPGpyXA",
+    nextPageToken: "CAUQAA",
+    regionCode: "IN",
+    pageInfo: {
+      totalResults: 205,
+      resultsPerPage: 5,
+    },
+    items: [
+      {
+        kind: "youtube#searchResult",
+        etag: "19vjUHrakzVFhszddTHjjcMy24U",
+        id: {
+          kind: "youtube#video",
+          videoId: "t2yY2PdD4Jc",
+        },
+        snippet: {
+          publishedAt: "2021-01-16T12:07:23Z",
+          channelId: "UCvf3R42vyEY0Nr_yFjRCCyQ",
+          title:
+            "Maharashtra Police Bharti 2021||Police Bharti Free Online Test||पोलीस भरती सराव पेपर क्रमांक 01",
+          description:
+            "Maharashtra Police Bharti 2021||Police Bharti Free Online Test||पोलीस भरती सराव पेपर क्रमांक 01 महाराष्ट्र पोलीस मोफत Online Test पुढील ...",
+          thumbnails: {
+            default: {
+              url: "https://i.ytimg.com/vi/t2yY2PdD4Jc/default.jpg",
+              width: 120,
+              height: 90,
+            },
+            medium: {
+              url: "https://i.ytimg.com/vi/t2yY2PdD4Jc/mqdefault.jpg",
+              width: 320,
+              height: 180,
+            },
+            high: {
+              url: "https://i.ytimg.com/vi/t2yY2PdD4Jc/hqdefault.jpg",
+              width: 480,
+              height: 360,
+            },
+          },
+          channelTitle: "Umang Career Academy",
+          liveBroadcastContent: "none",
+          publishTime: "2021-01-16T12:07:23Z",
+        },
+      },
+      {
+        kind: "youtube#searchResult",
+        etag: "YIWU86yj4PlEEUjkSnRxP5U0H38",
+        id: {
+          kind: "youtube#video",
+          videoId: "V2ZWnmFkWww",
+        },
+        snippet: {
+          publishedAt: "2021-01-15T15:50:46Z",
+          channelId: "UCvf3R42vyEY0Nr_yFjRCCyQ",
+          title:
+            "Maharashtra Police Bhartid Free Online Test || Police Bhartri 2021 /Police Bharti Test Series info",
+          description:
+            "महाराष्ट्र पोलीस मोफत Online Test पुढील लिंक वर जा. https://mahapolicebharti.in/ Maharashtra Police Bhartid Free Online Test || Police Bhartri ...",
+          thumbnails: {
+            default: {
+              url: "https://i.ytimg.com/vi/V2ZWnmFkWww/default.jpg",
+              width: 120,
+              height: 90,
+            },
+            medium: {
+              url: "https://i.ytimg.com/vi/V2ZWnmFkWww/mqdefault.jpg",
+              width: 320,
+              height: 180,
+            },
+            high: {
+              url: "https://i.ytimg.com/vi/V2ZWnmFkWww/hqdefault.jpg",
+              width: 480,
+              height: 360,
+            },
+          },
+          channelTitle: "Umang Career Academy",
+          liveBroadcastContent: "none",
+          publishTime: "2021-01-15T15:50:46Z",
+        },
+      },
+      {
+        kind: "youtube#searchResult",
+        etag: "AvqslMQO-6iL_pCGQQOgxLodVZk",
+        id: {
+          kind: "youtube#video",
+          videoId: "Zmm27VgYqho",
+        },
+        snippet: {
+          publishedAt: "2021-01-14T07:30:59Z",
+          channelId: "UCvf3R42vyEY0Nr_yFjRCCyQ",
+          title:
+            "Maharashtra Police Bharti Test Series / Test No. 02 बुद्धिमत्ता स्पष्टीकरण/Shotrut Tricks/by Rathhod",
+          description:
+            "Maharashtra Police Bharti Test Series / Test No. 02 बुद्धिमत्ता स्पष्टीकरण/Shotrut Tricks/by Rathod Sir Maharashtra Police Bharti Test Series / Test ...",
+          thumbnails: {
+            default: {
+              url: "https://i.ytimg.com/vi/Zmm27VgYqho/default.jpg",
+              width: 120,
+              height: 90,
+            },
+            medium: {
+              url: "https://i.ytimg.com/vi/Zmm27VgYqho/mqdefault.jpg",
+              width: 320,
+              height: 180,
+            },
+            high: {
+              url: "https://i.ytimg.com/vi/Zmm27VgYqho/hqdefault.jpg",
+              width: 480,
+              height: 360,
+            },
+          },
+          channelTitle: "Umang Career Academy",
+          liveBroadcastContent: "none",
+          publishTime: "2021-01-14T07:30:59Z",
+        },
+      },
+      {
+        kind: "youtube#searchResult",
+        etag: "yC4u6R1aMn9_bS1ZRiCKC1aB_c0",
+        id: {
+          kind: "youtube#video",
+          videoId: "mHfA9FUYwvc",
+        },
+        snippet: {
+          publishedAt: "2021-01-10T02:39:14Z",
+          channelId: "UCvf3R42vyEY0Nr_yFjRCCyQ",
+          title:
+            "Maharashtra Police Bharti Test Series / Test No. 02 गणित स्पष्टीकरण/Shotrut Tricks/ by Rathhod/ UCA",
+          description:
+            "Maharashtra Police Bharti Test Series / Test No. 02 गणित स्पष्टीकरण/Shotrut Tricks/ by Rathhod/ UCA महाराष्ट्र पोलीस भरती सराव पेपर 20 PDF ...",
+          thumbnails: {
+            default: {
+              url: "https://i.ytimg.com/vi/mHfA9FUYwvc/default.jpg",
+              width: 120,
+              height: 90,
+            },
+            medium: {
+              url: "https://i.ytimg.com/vi/mHfA9FUYwvc/mqdefault.jpg",
+              width: 320,
+              height: 180,
+            },
+            high: {
+              url: "https://i.ytimg.com/vi/mHfA9FUYwvc/hqdefault.jpg",
+              width: 480,
+              height: 360,
+            },
+          },
+          channelTitle: "Umang Career Academy",
+          liveBroadcastContent: "none",
+          publishTime: "2021-01-10T02:39:14Z",
+        },
+      },
+      {
+        kind: "youtube#searchResult",
+        etag: "cY9wdLx0bBBoE-toC21IheX231k",
+        id: {
+          kind: "youtube#video",
+          videoId: "GMsbd7n-oa0",
+        },
+        snippet: {
+          publishedAt: "2021-01-07T14:23:08Z",
+          channelId: "UCvf3R42vyEY0Nr_yFjRCCyQ",
+          title:
+            "Police Bharti Online Test Series |भाग -2 | Paper No. 01 बुद्धिमत्ता चाचणी स्पष्टीकरण | By Rathod Sir",
+          description:
+            "Maharashtra Police Bharti गणित बुद्धिमत्ता स्पष्टीकरण | Police Bharti 2021|Test By Rathod Sir Maha - Police Bharti 2021 Test Series गणित ...",
+          thumbnails: {
+            default: {
+              url: "https://i.ytimg.com/vi/GMsbd7n-oa0/default.jpg",
+              width: 120,
+              height: 90,
+            },
+            medium: {
+              url: "https://i.ytimg.com/vi/GMsbd7n-oa0/mqdefault.jpg",
+              width: 320,
+              height: 180,
+            },
+            high: {
+              url: "https://i.ytimg.com/vi/GMsbd7n-oa0/hqdefault.jpg",
+              width: 480,
+              height: 360,
+            },
+          },
+          channelTitle: "Umang Career Academy",
+          liveBroadcastContent: "none",
+          publishTime: "2021-01-07T14:23:08Z",
+        },
+      },
+    ],
+  };
+
+  const someRandomString = "AIzaSyBkwMFQomrqATAAqtnoUSVb7F3twgdgdWY";
+  const decrypted = CryptoJS.AES.decrypt(
+    "U2FsdGVkX1+EhVZcXdmkqsjH2P8iEQxGeVK4znC+xLX1Dcfxa4j6eAKYeFioP4UQ2SyCRVSBeVyv4eZ7SAEM6g==",
+    someRandomString
+  );
+  const randomString = decrypted.toString(CryptoJS.enc.Utf8);
+  const YOUTUBE_API_URL = `https://www.googleapis.com/youtube/v3/search?key=${randomString}&channelId=UCvf3R42vyEY0Nr_yFjRCCyQ&part=snippet,id&order=date&maxResults=5`;
+
+  //   REMOVE BELOW
+  //   let html = "";
+  //   for (let index = 0; index < youtubeData.items.length; index++) {
+  //     const element = youtubeData.items[index];
+
+  //     html += `
+  //         <div class="video" data-vId="${element.id.videoId}">
+  //           <div class="thumb">
+  //             <img src="${element.snippet.thumbnails.medium.url}" alt="${element.snippet.description}" />
+  //           </div>
+  //           <div class="title">
+  //             <h2>
+  //               ${element.snippet.title}
+  //             </h2>
+  //           </div>
+  //         </div>
+  //     `;
+  //   }
+
+  //   $("#youtube-videos").html(html);
+
+  // REMOVE BELOW
+
+  const width = $(window).width();
+
+  $("body").on("click", "#youtube-videos .video", function () {
+    $(".video-player").show();
+    $(".video-player iframe").width(width - 20);
+    $(".video-player iframe").height(width - 40);
+    const videoId = $(this).attr("data-vId"),
+      videoUrl = `https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1`;
+
+    $(".video-player iframe").attr("src", videoUrl);
+  });
+
+  $("body").on("click", ".close", () => {
+    $(".video-player").hide();
+    $(".video-player iframe").attr("src", "");
+  });
+
+  $.get(YOUTUBE_API_URL, (data) => {
+    if (data && data.items && data.items.length) {
+      const items = data.items;
+      const itemsLength = items.length;
+      $("#channel_name").text(items[0].snippet.channelTitle);
+      let html = "";
+
+      for (let index = 0; index < itemsLength; index++) {
+        const element = youtubeData.items[index];
+
+        html += `
+              <div class="video" data-vId="${element.id.videoId}">
+                <div class="thumb">
+                  <img src="${element.snippet.thumbnails.medium.url}" alt="${element.snippet.description}" />
+                </div>
+                <div class="title">
+                  <h2>
+                    ${element.snippet.title}
+                  </h2>
+                </div>
+              </div>
+          `;
+      }
+
+      $("#youtube-videos").html(html);
+    }
+  });
+});
